329f62184cf750d76fe3f218cbe2cb5900f82992
rckl0320 <130993661+rckl0320@users.noreply.github.com>
2023-05-30 09:04:18 -0400

    added movement mechanics

---
+++ Bear.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Character here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bear extends Actor
+{
+    /**
+     * Act - do whatever the Character wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("up"))
+        {
+            setRotation(270);
+            move(2);
+        }
+        if (Greenfoot.isKeyDown("down"))
+        {
+            setRotation(90);
+            move(2);
+        }
+        if (Greenfoot.isKeyDown("left"))
+        {
+            setRotation(180);
+            move(2);
+        }
+        if (Greenfoot.isKeyDown("right"))
+        {
+            setRotation(0);
+            move(2);
+        }
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Rick L
+ * @version May 30, 2023
  */
 public class MyWorld extends World
 {
@@ -17,5 +17,11 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        Bear sjo = new Bear();
+    
+        addObject(sjo, 200, 200);
     }
+    
+    
 }
