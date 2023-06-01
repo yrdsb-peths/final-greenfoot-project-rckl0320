69262ca54fc1f0321e2ad6d29a3fa4483cbc0172
rckl0320 <130993661+rckl0320@users.noreply.github.com>
2023-06-01 09:13:30 -0400

    changed bracket format

--- Chara.java
+++ Chara.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Character here.
+ *Player controlled character
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Rick L 
+ * @version June 2023
  */
 public class Chara extends Actor
 {
@@ -18,77 +18,66 @@ public class Chara extends Actor
         int tracker = 0;
         int rotation1 = 1;
         int rotation2 = 1;
+
         
         //collision detector
-        if(isTouching(Wall.class))
-        {
+        if(isTouching(Wall.class)){
             tracker = 1;
-            if(rotation1 == 1)
-            {
+            
+            if(rotation1 == 1){
                 rotation1 = getRotation();
             }
-            
         }
-        
-        else
-        {
-            if((getRotation() != 270) && (getRotation() != 90))
-            {
-                rotation1 = 1;
-                rotation2 = 1;
-            }
+        else{
+            //if((getRotation() != 270) && (getRotation() != 90)){
+            rotation1 = 1;
+            rotation2 = 1;
+            //}
         }
         
         //Movement and collision input
-        if (Greenfoot.isKeyDown("up"))
-        {
+        if (Greenfoot.isKeyDown("up")){
             setRotation(270);
-            
+
             rotation2 = getRotation();
- 
-            if(rotation2 != rotation1)
-            {
-    
+            
+            if(rotation2 != rotation1){
+                
                 move(3);
             }
             
         }
-        else if (Greenfoot.isKeyDown("down"))
-        {
+        else if (Greenfoot.isKeyDown("down")){
             setRotation(90);
             
             rotation2 = getRotation();
 
-            if(rotation2 != rotation1)
-            {   
+            if(rotation2 != rotation1){   
                 move(3);
             }
         }
-        else if (Greenfoot.isKeyDown("left"))
-        {
+        else if (Greenfoot.isKeyDown("left")){
             setRotation(180);
             
             rotation2 = getRotation();
 
-            if(rotation2 != rotation1)
-            {    
+            if(rotation2 != rotation1){
+                
                 move(3);
             }
         }
         
-        else if (Greenfoot.isKeyDown("right"))
-        {
+        else if (Greenfoot.isKeyDown("right")){
             setRotation(0);
             
             rotation2 = getRotation();
 
-            if(rotation2 != rotation1)
-            {
+            if(rotation2 != rotation1){            
                 move(3);
             }
         }
         
-        
+
     }
     
     public int getLocation()
